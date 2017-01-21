1a06ab4f1124eee0606ff2ed0fdd56d00d9e24ee
kesslern <nathant93@gmail.com>
2017-01-21 14:45:50 -0500

    Add sketch to test reading analog values

---
+++ raw-analog-read-test/raw-analog-read.ino
@@ -0,0 +1,64 @@
+/**
+ * State change detection (edge detection)
+ *
+ * The circuit:
+ * - VCC to one side of pushbutton 
+ * - pin 9 to other side of pushbutton
+ * - resistor from same side as pin 9 going to ground 
+ *
+ * A small capacitor can be added to both ends of the resistor
+ * to reduce bouncing.
+ * 
+ * Attach the analog read pin to the analog input source of interest. 
+ *
+ * Readings will be published when the button is pressed.
+ * 
+ **/
+
+#define DOWN false
+#define UP true
+
+#define LED_PIN 1 // built in LED
+#define BUTTON_PIN 9
+
+boolean buttonState = UP;
+boolean buttonStateChanged = false;
+
+void updateButtonState(boolean newState) {
+  
+  if (buttonState == newState) {
+    buttonStateChanged = false;
+  } else {
+    buttonStateChanged = true;
+    buttonState = newState;
+  }
+}
+
+
+void readButton() {
+
+  int buttonState = digitalRead(BUTTON_PIN);
+
+  if (buttonState == HIGH) {
+    updateButtonState(DOWN);
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    updateButtonState(UP);
+    digitalWrite(LED_PIN, LOW);
+  }
+}
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+void loop() {
+  readButton();
+
+  if (buttonStateChanged && buttonState == DOWN) {
+    Particle.publish("analog read", String(analogRead(A0)).c_str());
+  }
+}
+
